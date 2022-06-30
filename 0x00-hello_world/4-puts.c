45d19126e3fdf5f5721a1991ace6ddf2097bb666
ayanri <ishiekwenemercy@yahoo.com>
2022-06-30 12:29:20 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main(void) 
+/**
+ *
+ * main - Entry point
+ *
+ * Return: 0 after printing the function
+ */
+int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	return (0);
 }
