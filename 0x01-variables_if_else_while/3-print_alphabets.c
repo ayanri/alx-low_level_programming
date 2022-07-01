ff90fcec661665b588ce009bd45319f338e4c45b
Ayanri <ishiekwenemercy@yahoo.com>
2022-07-01 20:25:09 +0100

    alphABETS

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
- * main -a program that prints the alphabet in lowercase, and then in uppercase, followed by a new line
- *
- * Return: Always 0 (Success)
+ * main - main block
+ * Description:  prints the alphabet in lowercase,
+ * and then in uppercase, followed by a new line.
+ * Return: 0
  */
 int main(void)
 {
