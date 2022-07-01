5c7887943382f82d4f23ff31691bb2f71265a714
ayanri <ishiekwenemercy@yahoo.com>
2022-07-01 12:45:09 -0700

    Lowercase alphabet printing

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Desciption - a program that prints the alphabet in lowercase, followed by a new line
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+		char letter;
+
+		for (letter = 'a'; letter <= 'z'; letter++)
+		{
+		if (letter != 'e' && letter != 'q')
+		putchar(letter);
+		}
+		putchar('\n');
+
+	return (0);
+}
