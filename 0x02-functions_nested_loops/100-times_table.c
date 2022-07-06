705ea7937a0d2d02d00eb635daa2247be533de0f
Ayanri <ishiekwenemercy@yahoo.com>
2022-07-06 09:58:33 +0100

    prints the n times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,47 +1,66 @@
-#inchude "main.h"
+#include "main.h"
+
+int _putchar(char c);
 
 /**
  * print_times_table - prints the n times table, starting with 0
- * @n: number of times table
+ *
+ * @n: n times table
+ *
+ * Return: No return
  */
+
 void print_times_table(int n)
 {
-	int x, y, z
+	int x;
+	int y;
 
-		if (n >= 0 && n <= 15)
+	if (n >= 0 && n <= 15)
+	{
+
+		for (x = 0; x <= n; x++)
 		{
-			for (x = 0; x <= n; x++)
+
+			for (y = 0; y <= n; y++)
 			{
-				for (y = 0; y <= n; y++)
+
+				if ((x * y) < 10)
 				{
-					z = y * x;
-					if (y == 0)
+					if (y != 0)
 					{
-						_putchar(z + '0');
-					} else if (z < 10 && y != 0)
-					{
-						_putchar(',');
 						_putchar(' ');
 						_putchar(' ');
 						_putchar(' ');
-						_putchar(z + '0');
-					} else if (z >= 10 && z < 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar(' ');
-						_putchar((z / 10) + '0');
-						_putchar((z % 10) + '0');
-					} else if (z >= 100)
-					{
-						_putchar(',');
-						_putchar(' ');
-						_putchar((z / 100) + '0');
-						_putchar(((z / 10) % 10) + '0');
-						_putchar((z % 10) + '0');
 					}
+					_putchar((y * x) + '0');
 				}
-				_putchar('\n');
+
+				else if ((x * y) < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar((((y * x) / 10) % 10) + '0');
+					_putchar(((y * x) % 10) + '0');
+				}
+
+				else if ((x * y) < 1000)
+				{
+					_putchar(' ');
+					_putchar((((y * x) / 100) % 10) + '0');
+					_putchar((((y * x) / 10) % 10) + '0');
+					_putchar(((y * x) % 10) + '0');
+				}
+			if (y != (n))
+			{
+				_putchar(',');
 			}
+
+			}
+
+		_putchar('\n');
+
 		}
+
+	}
+
 }
