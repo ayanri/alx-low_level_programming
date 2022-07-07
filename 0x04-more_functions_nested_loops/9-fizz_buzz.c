649be80fc48a804cf698bdb47c6d3a008169eaeb
ayanri <ishiekwenemercy@yahoo.com>
2022-07-07 15:35:29 -0700

    More functions, More nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	int c = 1;
+
+	while (c <= 100)
+	{
+		if (c % 3 == 0 && c % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+
+		else if (c % 3 == 0)
+		{
+			printf("Fizz");
+		}
+
+		else if (c % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", c)
+		}
+		if (c != 100)
+		{
+			putchar(' ');
+		}
+
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}
