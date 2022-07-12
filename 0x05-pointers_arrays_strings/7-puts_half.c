810b4984a3290465dbd13dfc8991330c4f38e9f2
ayanri <ishiekwenemercy@yahoo.com>
2022-07-12 05:59:34 -0700

    C - POinters, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * @str: char array string type
+ */
+
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+		a++;
+	for (a /= 2; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
