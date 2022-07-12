0ca1c72100d99ef9f646a28a0d5db56d3d243604
ayanri <ishiekwenemercy@yahoo.com>
2022-07-12 05:22:38 -0700

    C - Pointers, Arrays and Strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string to stdout
+ * @str: Parameter
+ *
+ * Return: void
+ */
+
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
