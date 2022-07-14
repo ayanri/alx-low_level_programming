810037f6282f327e0e522db1a5b8d86f779ae3d7
ayanri <ishiekwenemercy@yahoo.com>
2022-07-14 15:33:02 -0700

    more pointers, arrays and strings

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -4,20 +4,17 @@
  * string_toupper - changes all lowercase letters of a string to uppercase
  * @str: string to capatilize
  *
- * Return: Always 0
+ * Return: capitalized string
  */
 
 char *string_toupper(char *str)
 {
-	int c = 0;
+	int x;
 
-	while (str[c] != '\0')
+	for (x = 0; str[x] != '\0'; x++)
 	{
-		if ((str[c] <= 97) && (str[c] >= 122))
-		{
-			str[c] = str[c] - 32;
-		}
-	c++;
+		if (str[x] <= 'z' && str[x] >= 'z')
+			str[x] -= 32;
 	}
 	return (str);
 }
