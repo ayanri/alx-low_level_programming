c5284f2ab94526f529796049222bac56641a49cd
ayanri <ishiekwenemercy@yahoo.com>
2022-07-22 06:21:28 -0700

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints name of file
+ * @argc: argument count
+ * @argv: argument array value
+ *
+ * Return: 1
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
