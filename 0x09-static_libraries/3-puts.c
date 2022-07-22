b81f66a8c9a69bd4954967a10551e011db9e2864
ayanri <ishiekwenemercy@yahoo.com>
2022-07-22 11:39:05 -0700

    C static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,1 @@
-#include "main.h"
-
-/**
- * _puts - prints a string to stdout
- * @str: Parameter
- *
- * Return: void
- */
-
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
+void _puts(char *str) {}
