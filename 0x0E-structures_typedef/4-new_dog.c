8939598326aeb2ad9edbaeffbe84dbbe536629a0
ayanri <ishiekwenemercy@yahoo.com>
2022-08-01 16:04:06 -0700

    structures_typedef

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,72 +14,44 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newdog;
-	int new_name;
-	int new_owner;
+	int a = 0;
+	int b = 0;
+	int c;
+
+	while (name[a] != '\0')
+		a++;
+	while (owner[b] != '\0')
+		b++;
 
 	newdog = malloc(sizeof(dog_t));
 	if (newdog == NULL)
-		return (NULL);
-
-	new_name = _strlen(name);
-	newdog->name = malloc(sizeof(char) * new_name + 1);
-	if (newdog->name == NULL)
 	{
 		free(newdog);
 		return (NULL);
 	}
 
-	newdog->name = _strcpy(newdog->name, name);
-	new_owner = _strlen(owner);
-	newdog->owner = malloc(sizeof(char) * new_owner + 1);
-	if (newdog->owner == NULL)
+	newdog->name = malloc(a * sizeof(newdog->name));
+	if (newdog->name == NULL)
 	{
 		free(newdog->name);
 		free(newdog);
 		return (NULL);
 	}
 
-	newdog->owner = _strcpy(newdog->owner, owner);
+	for (c = 0; c <= a; c++)
+		newdog->name[c] = name[c];
+
 	newdog->age = age;
-
-	return (newdog);
-}
-
-/**
- * _strlen - determines the length of a string
- * @s: pointer
- *
- * Return: the length
- */
-
-int _strlen(char *s)
-{
-	int a;
-
-	for (a = 0; s[a] != '\0', a++)
-		;
-	return (a);
-}
-
-/**
- * *_strcpy - copies a pointer to a string
- * description: returns value of dest
- * @dest: pointer to destination
- * @src: pointer to source
- *
- * return: pointer
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int a = 0;
-
-	while (src[a] != '\0')
+	newdog->owner = malloc(b * sizeof(newdog->owner));
+	if (newdog->owner == NULL)
 	{
-		dest[a] = src[a];
-		a++;
+		free(newdog->owner);
+		free(newdog->name);
+		free(newdog);
+		return (NULL);
 	}
 
-	dest[a] = '\0';
-	return (dest);
+	for (c = 0; c <= b; c++)
+		newdog->owner[c] = owner[c];
+	return (newdog);
 }
