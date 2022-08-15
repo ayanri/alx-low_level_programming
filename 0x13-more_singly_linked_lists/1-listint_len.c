40d66dc25c552bcc8d307046ca86b3591dff8e18
ayanri <ishiekwenemercy@yahoo.com>
2022-08-15 14:20:54 -0700

    more singly linked lists

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,21 +1,18 @@
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a listint_t list
+ * listint_len - returns the number of elements in a linked listint_t list
  * @h: struct data type pointer
  *
- * Return: number of nodes
+ * Return: number of element linked
  */
 
-size_t print_listint(const listint_t *h)
+size_t listint_len(const listint_t *h)
 {
 	unsigned int a = 0;
 	const listint_t	 *new_node = h;
 
 	for (; new_node; new_node = new_node->next)
-	{
-		printf("%d\n", new_node->n);
 		a++;
-	}
 	return (a);
 }
