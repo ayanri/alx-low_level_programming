226faad7e84a2d1596d3a7f338a4002978878566
ayanri <ishiekwenemercy@yahoo.com>
2022-08-18 13:26:52 -0700

    bit manipulation

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,4 +1,16 @@
 #include "main.h"
+/**
+ * _right_pbin_mask - recursion function to print bit 0
+ * @n: pointer
+ */
+
+void _right_pbin_mask(unsigned long int n)
+{
+	if (n == 0)
+		return;
+	_right_pbin_mask(n >> 1);
+	_putchar((n & 1) + '0');
+}
 
 /**
  * print_binary - prints the binary representation of a number
@@ -9,14 +21,8 @@
 
 void print_binary(unsigned long int n)
 {
-	if (n < 1)
-	{
-		print_binary(n >> 1);
-	}
-	if (n & 1)
-	{
-		_putchar('1');
-	}
-	else
+	if (n == 0)
 		_putchar('0');
+	else
+		_right_pbin_mask(n);
 }
